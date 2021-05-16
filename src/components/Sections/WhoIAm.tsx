99d2f8eff7f0fb00725a1dab7f3fb5c7fd61e5d8
jllopiz90 <jesusllopiz90@gmail.com>
2021-05-15 23:25:54 -0400

    build: mobile version until now

--- src/components/Sections/WhoIAm.tsx
+++ src/components/Sections/WhoIAm.tsx
@@ -7,7 +7,40 @@ import "./whoiam.css";
 
 const WhoIAm = () => {
   const breakpoints = useBreakpoint();
-  return (
+
+  const mobileSection = () => (
+    <>
+      <div className="full-row">
+        <a
+          href="https://github.com/jllopiz90"
+          target="_new"
+        >
+          <img src={MeCoding} width="120px" alt="avatar" className="grow" />
+        </a>
+      </div>
+      <div className="full-row">
+        <ul className="circle">
+          <li>👨‍👩‍👧 Father of 1 lovely girl and husband of a wonderful woman.</li>
+          <li>🇨🇺 Cuban born in 1990.</li>
+          <li>🇺🇸 US resident since late 2015.</li>
+          <li>🤖 Logical thinker</li>
+          <li>
+            👨🏽‍🎓 Computer Science graduate in the University of Havana in 2014
+          </li>
+          <li>
+            👨🏽‍💻 Learned to code with C#, worked in web applications with PHP, in
+            love with JS because of React ⚛️{" "}
+          </li>
+          <li>
+            🎮 Gamer for fun 📚 I have to admit I use to read a lot more than I
+            do nowdays ⚽ Love european football.{" "}
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+
+  const regularSection = () => (
     <>
       <div
         className="fadeLeftContainer"
@@ -25,13 +58,13 @@ const WhoIAm = () => {
         <ul className="circle">
           <li>👨‍👩‍👧 Father of 1 lovely girl and husband of a wonderful woman.</li>
           <li>🇨🇺 Cuban born in 1990.</li>
-          <li>🇺🇸 US resident since 2017.</li>
+          <li>🇺🇸 US resident since 2015.</li>
           <li>🤖 Logical thinker</li>
           <li>
             👨🏽‍🎓 Computer Science graduate in the University of Havana in 2014
           </li>
           <li>
-            👨🏽‍💻 Learned coding with C#, worked in web applications with PHP, in
+            👨🏽‍💻 Learned to code with C#, worked in web applications with PHP, in
             love with JS because of React ⚛️{" "}
           </li>
           <li>
@@ -42,6 +75,8 @@ const WhoIAm = () => {
       </div>
     </>
   );
+
+  return breakpoints.sm ? mobileSection() : regularSection();
 };
 
 export default WhoIAm;
